bdf90d568dfa629b853d03cd150970fdab246322
skipper20181111 <17854230845@163.com>
2022-12-05 16:15:24 +0800

    增加一个getaddress接口的未找到数据err判断

--- internal/logic/address/getaddressLogic.go
+++ internal/logic/address/getaddressLogic.go
@@ -3,6 +3,7 @@ package address
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"oa_final/internal/svc"
 	"oa_final/internal/types"
@@ -26,6 +27,11 @@ func NewGetaddressLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Getadd
 
 func (l *GetaddressLogic) Getaddress(req *types.GetAddressRes) (resp *types.GetAddressResp, err error) {
 	findAddressListByPhone, err := l.svcCtx.UserAddressString.FindOneByPhone(l.ctx, req.Phone)
+	if err != nil {
+		fmt.Println("##############", err.Error())
+		return &types.GetAddressResp{Code: "4004", Msg: "没有找到数据", Data: &types.GetAddressRp{}}, nil
+
+	}
 	addressList := make([]*types.AddressInfo, 0)
 	json.Unmarshal([]byte(findAddressListByPhone.AddressString), &addressList)
 	return &types.GetAddressResp{Code: "10000", Msg: "success", Data: &types.GetAddressRp{Address: addressList}}, nil

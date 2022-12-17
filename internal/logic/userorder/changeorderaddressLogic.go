76585f324e03d38f4ce8564b06db20052ea83b1a
skipper20181111 <17854230845@163.com>
2022-12-17 16:17:51 +0800

    订单接口还差最后一个退款接口

--- internal/logic/userorder/changeorderaddressLogic.go
+++ internal/logic/userorder/changeorderaddressLogic.go
@@ -2,6 +2,9 @@ package userorder
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"time"
 
 	"oa_final/internal/svc"
 	"oa_final/internal/types"
@@ -24,7 +27,21 @@ func NewChangeorderaddressLogic(ctx context.Context, svcCtx *svc.ServiceContext)
 }
 
 func (l *ChangeorderaddressLogic) Changeorderaddress(req *types.ChangeOrdeRaddressRes) (resp *types.ChangeOrdeRaddressResp, err error) {
-	// todo: add your logic here and delete this line
+	sn2order, err := l.svcCtx.UserOrder.FindOneByOrderSn(l.ctx, req.OrderSn)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	addr, err := json.Marshal(req.Address)
+	if err != nil {
+		fmt.Println(err.Error(), "结构体转化为字符串失败")
+	}
+	sn2order.ReceiverInfo = string(addr)
+	sn2order.ModifyTime = time.Now()
+	err = l.svcCtx.UserOrder.Update(l.ctx, sn2order)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	sn, err := l.svcCtx.UserOrder.FindOneByOrderSn(l.ctx, req.OrderSn)
 
-	return
+	return &types.ChangeOrdeRaddressResp{Code: "10000", Msg: "修改成功", Data: &types.ChangeOrdeRaddressRp{OrderInfo: db2orderinfo(sn)}}, nil
 }
--- internal/logic/userorder/getorderLogic.go
+++ internal/logic/userorder/getorderLogic.go
@@ -2,6 +2,10 @@ package userorder
 
 import (
 	"context"
+	"fmt"
+	"github.com/wechatpay-apiv3/wechatpay-go/core"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/jsapi"
+	"time"
 
 	"oa_final/internal/svc"
 	"oa_final/internal/types"
@@ -24,7 +28,36 @@ func NewGetorderLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Getorder
 }
 
 func (l *GetorderLogic) Getorder(req *types.GetOrderRes) (resp *types.GetOrderResp, err error) {
-	// todo: add your logic here and delete this line
+	sn2order, err := l.svcCtx.UserOrder.FindOneByOrderSn(l.ctx, req.OrderSn)
+	if err != nil {
+		return &types.GetOrderResp{Code: "4004", Msg: err.Error()}, nil
+	}
+	if sn2order.OrderStatus == 0 { // 说明还没有付款，去查一查究竟有没有付款
+		jssvc := jsapi.JsapiApiService{Client: l.svcCtx.Client}
+		no2payment, _, err := jssvc.QueryOrderByOutTradeNo(l.ctx, jsapi.QueryOrderByOutTradeNoRequest{
+			OutTradeNo: core.String("PIS5TwTkh3Z0vv5G3BzT2NLorgVQi52P"),
+			Mchid:      core.String(l.svcCtx.Config.WxConf.MchID)})
+		if err != nil {
+			return &types.GetOrderResp{Code: "4004", Msg: err.Error()}, nil
+		}
+		if *no2payment.TradeState != "SUCCESS" {
+			return &types.GetOrderResp{Code: "10000", Msg: "查询成功", Data: &types.GetOrderRp{OrderInfo: db2orderinfo(sn2order)}}, nil
+		} else {
+			sn2order.OrderStatus = 1
+			sn2order.ModifyTime = time.Now()
+			sn2order.PaymentTime = sn2order.ModifyTime
+			err := l.svcCtx.UserOrder.Update(l.ctx, sn2order)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			sn, err := l.svcCtx.UserOrder.FindOneByOrderSn(l.ctx, sn2order.OrderSn)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			return &types.GetOrderResp{Code: "10000", Msg: "查询成功", Data: &types.GetOrderRp{OrderInfo: db2orderinfo(sn)}}, nil
+		}
+	}
 
-	return
+	return &types.GetOrderResp{Code: "10000", Msg: "查询成功", Data: &types.GetOrderRp{OrderInfo: db2orderinfo(sn2order)}}, nil
+
 }

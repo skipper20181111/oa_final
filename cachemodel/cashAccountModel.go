dfe88bbbd459b1d000504964ecfbcbede3a0d837
skipper20181111 <17854230845@163.com>
2023-02-02 17:04:09 +0800

    初步修改订单模块

---
+++ cachemodel/cashAccountModel.go
@@ -0,0 +1,24 @@
+package cachemodel
+
+import "github.com/zeromicro/go-zero/core/stores/sqlx"
+
+var _ CashAccountModel = (*customCashAccountModel)(nil)
+
+type (
+	// CashAccountModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customCashAccountModel.
+	CashAccountModel interface {
+		cashAccountModel
+	}
+
+	customCashAccountModel struct {
+		*defaultCashAccountModel
+	}
+)
+
+// NewCashAccountModel returns a model for the database table.
+func NewCashAccountModel(conn sqlx.SqlConn) CashAccountModel {
+	return &customCashAccountModel{
+		defaultCashAccountModel: newCashAccountModel(conn),
+	}
+}

3c7298bb3889e2e403a2e0e75a0567e230b07f54
skipper20181111 <17854230845@163.com>
2023-06-04 16:18:41 +0800

    starorder

---
+++ cachemodel/couponModel.go
@@ -0,0 +1,27 @@
+package cachemodel
+
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+var _ CouponModel = (*customCouponModel)(nil)
+
+type (
+	// CouponModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customCouponModel.
+	CouponModel interface {
+		couponModel
+	}
+
+	customCouponModel struct {
+		*defaultCouponModel
+	}
+)
+
+// NewCouponModel returns a model for the database table.
+func NewCouponModel(conn sqlx.SqlConn, c cache.CacheConf) CouponModel {
+	return &customCouponModel{
+		defaultCouponModel: newCouponModel(conn, c),
+	}
+}

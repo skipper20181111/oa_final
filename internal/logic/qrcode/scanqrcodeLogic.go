1317951942d4302b4d34f7034b4f9db061f7b6b9
skipper20181111 <17854230845@163.com>
2023-06-26 10:16:41 +0800

    重构coupon和扫码，并放入oa中

--- internal/logic/qrcode/scanqrcodeLogic.go
+++ internal/logic/qrcode/scanqrcodeLogic.go
@@ -28,20 +28,22 @@ func NewScanqrcodeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Scanqr
 }
 
 func (l *ScanqrcodeLogic) Scanqrcode(req *types.ScanQRcodeRes) (resp *types.ScanQRcodeResp, err error) {
+	data := &types.ScanQRcodeRp{Type: "unknow", Msg: randStr(10)}
 	//voucher券的parameter1是vid，old2new的券是老用户手机号
 	decrypt, err := coupon.Decrypt(req.QRcodeMsg, svc.Keystr)
 	if err != nil || decrypt == "" {
-		return &types.ScanQRcodeResp{Code: "10000", Msg: "请检查二维码或1分钟后再试"}, nil
+		return &types.ScanQRcodeResp{Code: "10000", Msg: "请检查二维码或1分钟后再试", Data: data}, nil
 	}
 	QRCodeMsg := &types.QrCode{}
 	err = json.Unmarshal([]byte(decrypt), QRCodeMsg)
 	if err != nil {
-		return &types.ScanQRcodeResp{Code: "10000", Msg: "请检查二维码或1分钟后再试"}, nil
+		return &types.ScanQRcodeResp{Code: "10000", Msg: "请检查二维码或1分钟后再试", Data: data}, nil
 	}
 	switch QRCodeMsg.Type {
 	case "voucher":
 		_, msg := l.vu.VoucherbindByVid(QRCodeMsg)
-		return &types.ScanQRcodeResp{Code: "10000", Msg: msg}, nil
+		data.Type = "voucher"
+		return &types.ScanQRcodeResp{Code: "10000", Msg: msg, Data: data}, nil
 	}
-	return &types.ScanQRcodeResp{Code: "10000", Msg: "请检查二维码"}, nil
+	return &types.ScanQRcodeResp{Code: "10000", Msg: "请检查二维码", Data: data}, nil
 }
